cb1e00056a83dbb0d6a5f32847b4f122c208ad59
fredric47 <lpratt1@byupathway.edu>
2026-02-19 16:26:18 -0700

    fixed fact button

--- project/scripts/knights-otsb.js
+++ project/scripts/knights-otsb.js
@@ -5,17 +5,14 @@ currentYear.innerHTML = `<span>${today.getFullYear()}</span>`;
 const lastModified = document.getElementById("lastModified").innerHTML = document.lastModified;
 
 let chessFacts = [
-    "0Chess is over 1000 years old",
-    "1Chess is over 1000 years old",
-    "2Chess is among the oldest games in the world",
-    "3Chess is among the oldest games in the world",
-    "4Chess is one of the most reconigisable games world wide",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+    "The new pawn move, where pawns were allowed to advance two squares on their first move instead of one, was first introduced in Spain in 1280.",
+    "There are well over 1,000 different openings, including variations within larger openings/defenses that one can learn.",
+    "Chess sets normally have 32 pieces. But it has become popular to include 2 extra queens making it 34. The two spare queens can be used in pawn promotion.",
+    "The word “gambit” is specifically a chess term, now generalized to mean a 'sacrifice for advantage'.",
+    "The longest recorded tournament chess match lasted 20 hours and 15 minutes and resulted in a draw.",
+    "The number of possibilities of a Knight's tour is over 122 million.",
+    "The word 'Checkmate' in Chess comes from the Persian phrase 'Shah Mat,' which means 'the King is dead'.",
+    "The longest chess game theoretically possible is 5,949 moves."
 ]
 
 let index = localStorage.getItem('currentFact')
@@ -24,19 +21,23 @@ if (index === null) {
 }
 
 
-function nextFact(factArray, factIndex) {
-    if (factIndex >= factArray.length) {
-        localStorage.setItem('currentFact', '0');
-        factIndex = 0;
-    };
-    return chessFacts[factIndex];
-}
+// function nextFact(factIndex) {
+//     let fact = factIndex;
+
+//     return chessFacts[fact];
+// }
 
 function funFact() {
     let factIndex = JSON.parse(localStorage.getItem('currentFact')); //remember.getItem //copilot
     const funFactArea = document.getElementById("factarea");
-    funFactArea.innerHTML = nextFact(chessFacts, factIndex);
+    funFactArea.innerHTML = chessFacts[factIndex];
     factIndex++;
+
+    if (factIndex >= chessFacts.length) {
+        localStorage.setItem('currentFact', '0');
+        factIndex = 0;
+    };
+
     localStorage.setItem('currentFact', factIndex.toString());
 }
 
